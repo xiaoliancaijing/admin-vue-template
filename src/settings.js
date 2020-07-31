7fdad5c201a32f6df254a7d744a820b3b3e73fd6
c.k <ck.email2010@gmail.com>
2020-07-31 12:22:22 +0800

    环境配置

--- src/settings.js
+++ src/settings.js
@@ -2,22 +2,25 @@
  * @Author: wei.chen
  * @Date: 2020-06-11 12:42:39
  * @LastEditors: wei.chen
- * @LastEditTime: 2020-06-11 12:54:13
- * @Descripttion:
+ * @LastEditTime: 2020-07-31 12:21:34
+ * @Descripttion: 项目的配置文件，主要是配置惨淡 头部logo 等
  */
 
 module.exports = {
+	/**
+	 *  项目的默认标题
+	 */
 	title: 'Admin',
 
 	/**
 	 * @type {boolean} true | false
-	 * @description Whether fix the header
+	 * @description 固定顶部
 	 */
 	fixedHeader: true,
 
 	/**
 	 * @type {boolean} true | false
-	 * @description Whether show the logo in sidebar
+	 * @description 侧边栏是否显示logo
 	 */
 	sidebarLogo: true,
-};
+}
