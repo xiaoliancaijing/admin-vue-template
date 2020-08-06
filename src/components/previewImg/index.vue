8e2f853696b488ac19b0c59e85cc2370ae49f24d
tianhao <tianhao>
2020-08-06 12:39:37 +0800

    修改图片预览组件样式

--- src/components/previewImg/index.vue
+++ src/components/previewImg/index.vue
@@ -2,7 +2,7 @@
  * @Author: hao.tian
  * @Date: 2020-08-01 15:31:00
  * @LastEditors: hao.tian
- * @LastEditTime: 2020-08-01 16:35:10
+ * @LastEditTime: 2020-08-06 12:41:04
  * @Descripttion: 图片预览 
     params: 
         title: String 标题 默认为'预览'
@@ -13,8 +13,7 @@
 -->
 <template>
 	<el-dialog :visible.sync="isShowImageDialog" @closed="clearImg">
-		<el-carousel indicator-position="outside" height="600px"></el-carousel>
-		<el-carousel :interval="4000" type="card" height="200px">
+		<el-carousel :interval="4000" type="card" height="600px">
 			<el-carousel-item v-for="item in imgs" :key="item">
 				<img v-bind:src="item" />
 			</el-carousel-item>
